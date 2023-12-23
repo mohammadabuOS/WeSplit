a45144e7e5c39cde13510c3b396c0ced5c54ad91
mohammadabuOS <moe.abuosbie@gmail.com>
2023-12-22 21:53:58 -0600

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,26 @@
+// NAME: WeSplit
+// AUTHOR: Mohammad Abuosbie
+// DESCRIPTION:
+//  A check-splitting app. Users need to be able to enter the cost of their check, how many people are sharing the cost, and how much tip they want to leave.
+
+import SwiftUI
+
+struct ContentView: View {
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mohammad Abuosbie on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
