9f64e1709a3d265363afabd442c3e14d0c3df16c
mohammadabuOS <moe.abuosbie@gmail.com>
2023-12-26 20:02:47 -0600

    Complete section: Reading text from the user with TextField

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
         Form {
             Section {
                 TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
             }
         }
     }
